3491d206fa1ecee74ed22799473b7a288b2ffa41
itsactuallyjw <itsactuallyjw@gmail.com>
2024-06-15 12:56:56 +0800

    Initial commit: Added index.html, game.js, and assets

---
+++ node_modules/game.js
@@ -0,0 +1,43 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+let platforms;
+
+function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create() {
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    const player = this.physics.add.sprite(100, 450, 'player');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+}
+
+function update() {
+    // Game update logic
+}
